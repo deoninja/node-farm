f7b07f94a8ab238ac82df124bf64d0edfd8ad5f3
deoninja <125787850+deoninja@users.noreply.github.com>
2023-08-11 13:39:53 +0800

    slugify
    add 3rd party package

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-
+const slugify = require("slugify");
 const replaceTemplate = require("./modules/replaceTemplate");
 
 //////////////////////////////////////
@@ -46,9 +46,13 @@ const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
   "utf-8"
 );
-
+//TODO: FIXME:
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+const slug = dataObj.map((el) => slugify(el.productName, { lower: true }));
+
+console.log(slug);
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
